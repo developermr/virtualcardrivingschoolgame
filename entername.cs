9e16b889334cd73281b641893eac11902ef7a092
Mr Developer <67146813+developermr@users.noreply.github.com>
2021-07-19 14:06:32 +0500

    Add files via upload

---
+++ entername.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class entername : MonoBehaviour
+{
+
+    public GameObject Ruleviolationcontainer;
+    public GameObject Namecontainer;
+    public static string userentername;
+    //  public Text Username;
+ 
+
+    // Start is called before the first frame update
+
+
+    public void readtext(string s)
+    {
+        userentername = s;
+        Namecontainer.SetActive(false);
+        Ruleviolationcontainer.SetActive(true);
+    }
+
+    public void onpressok()
+    {
+        Namecontainer.SetActive(false);
+      //  Debug.Log(userentername);
+        Ruleviolationcontainer.SetActive(true);
+    }
+
+
+}
---
+++ ruledetails.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ruledetails : MonoBehaviour
+{
+
+    public Text r1text;
+    public Text r2text;
+    public Text r3text;
+    public Text r4text;
+    public Text r5text;
+    public Text r6text;
+    public Text username;
+    public Text score;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        r1text.text = playermovement.r1;
+        r2text.text = playermovement.r2;
+        r3text.text = playermovement.r3;
+        r4text.text = playermovement.r4;
+        r5text.text = playermovement.r5;
+        r6text.text = GameUI.r6;
+        username.text = entername.userentername;
+        int userscore = playermovement.userscore;
+        score.text = userscore.ToString();
+
+    }
+
+    // Update is called once per frame
+    public GameObject highscorecontainer;
+    public GameObject ruleviewcontainer;
+    public void onnextpress()
+    {
+
+        ruleviewcontainer.SetActive(false);
+        highscorecontainer.SetActive(true);
+    }
+}
